adce617bf4961e140b5242443ed12dff4125f923
domc1o <ddziag@s-dc.eu>
2017-06-27 11:08:24 +0200

    Some first tests :)

--- test/ExpressApp.test.ts
+++ test/ExpressApp.test.ts
@@ -5,21 +5,45 @@ import app from '../src/ExpressApp';
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('baseRoute', () => {
+describe('GET /api/v1/promatches', () => {
     it('should be json', () => {
         return chai
             .request(app)
-            .get('/')
+            .get('/api/v1/promatches')
             .then(res => {
                 expect(res.type).to.eql('application/json');
             });
     });
-    it('should have a message prop', () => {
+    it('should be array', () => {
         return chai
             .request(app)
-            .get('/')
+            .get('/api/v1/promatches')
             .then(res => {
-                expect(res.body.message).to.eql('Hello World!');
+                expect(res.type).to.eql('application/json');
             });
+    });
+
+    it('there should be atleast 1 match info', () => {
+        return chai
+            .request(app)
+            .get('/api/v1/promatches')
+            .then(res => {
+                let matchInfo = res.body.shift();
+                expect(matchInfo).to.exist;
+                expect(matchInfo).to.have.all.keys([
+                    "match_id",
+                    "duration",
+                    "start_time",
+                    "radiant_team_id",
+                    "radiant_name",
+                    "dire_team_id",
+                    "dire_name",
+                    "leagueid",
+                    "league_name",
+                    "series_id",
+                    "series_type",
+                    "radiant_win"
+                ]);
+            })
     });
 });
